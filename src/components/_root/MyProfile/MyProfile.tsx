71fd167a78e3ac25244e76d2eae082694223ab5f
Oleksandr Kuchera <116847212+OleksandrKuchera@users.noreply.github.com>
2024-03-21 22:24:13 +0200

    Update MyProfile.tsx

--- src/components/_root/MyProfile/MyProfile.tsx
+++ src/components/_root/MyProfile/MyProfile.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 interface UserInfo {
-  username: string;
+  name: string;
   email: string;
   avatar: string | null;
   // Додайте інші властивості, якщо потрібно
