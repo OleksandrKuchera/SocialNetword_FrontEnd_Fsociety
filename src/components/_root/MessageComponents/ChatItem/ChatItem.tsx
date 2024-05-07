8add7a375ce1fbb02368d30a5cbfea4fbd738052
staff <masterb21052017@gmail.com>
2024-05-07 13:53:48 +0300

    fix avatar in message

--- src/components/_root/MessageComponents/ChatItem/ChatItem.tsx
+++ src/components/_root/MessageComponents/ChatItem/ChatItem.tsx
@@ -13,17 +13,21 @@ const ChatItem = ({ isActive, onClick, user, }: chatItemType) => {
 
     return (
         <div className={isActive ? `${style.active} ${style.chat__item}}` : `${style.chat__item}`} onClick={onClick}>
-            <div className="d-flex justify-content-between">
-                <div>
-                    <div className={style.avatar__info}>
-                        <img src={user.avatar.slice(13)} alt="avatar" />
-                        <div>
-                            <h4>{user.name}</h4>
+            <div className={style.avatar__info}>
+                <div className="row d-flex align-items-center">
+                    <div className="col-7">
+                        <div className="row d-flex align-items-center">
+                            <div className="col-6">
+                                <img src={user.avatar.slice(13)} alt="avatar" />
+                            </div>
+                            <div className="col-6">
+                                <h4>{user.name}</h4>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className='d-flex align-items-center'>
-                    {user.bio.trim().length > 0 ? <p className={style.chat__message}>{shortenText(user.bio, 18)}</p> : null}
+                    <div className="col-5">
+                        {user.bio.trim().length > 0 ? <p className={style.chat__message}>{shortenText(user.bio, 18)}</p> : null}
+                    </div>
                 </div>
             </div>
         </div>
