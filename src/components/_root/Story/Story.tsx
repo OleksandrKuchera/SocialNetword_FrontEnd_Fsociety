16d68fab8f44891b41c2ad973acdc9520140145a
staff <masterb21052017@gmail.com>
2024-05-16 23:27:28 +0300

    add story in home

--- src/components/_root/Story/Story.tsx
+++ src/components/_root/Story/Story.tsx
@@ -1,5 +1,5 @@
 import { Avatar } from '@mui/material';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './StoryReel.scss';
 import { Author } from '../Home/Home';
 import { StoryAsk } from '.';
@@ -8,17 +8,25 @@ import { getVideoThumbnail } from '../functions/getVideoThumbnail';
 interface StoryProps {
   author: Author,
   story: StoryAsk,
+  onClick: () => void,
 }
 
 function Story(props: StoryProps): React.ReactElement {
-  const { story, author } = props;
-  const thumbnailUrl = getVideoThumbnail(story.media);
+  const { story, author, onClick } = props;
+  const [thumbnail, setThumbnail] = useState<string>('');
+
+  useEffect(() => {
+    getVideoThumbnail(story.media)
+      .then((thumb) => setThumbnail(thumb as string))
+      .catch((e) => console.error('Error generating thumbnail:', e));
+  }, [story.media]);
 
   return (
     <div
       className='story'
+      onClick={onClick}
       style={{
-        backgroundImage: `url(${thumbnailUrl})`,
+        backgroundImage: `url(${thumbnail})`,
         backgroundRepeat: 'no-repeat',
       }}
     >
--- src/components/_root/Story/index.tsx
+++ src/components/_root/Story/index.tsx
@@ -5,6 +5,7 @@ import { userDataType } from '../HomeLayout/HomeLayout';
 import { useEffect, useState } from 'react';
 import { Author } from '../Home/Home';
 import axios from 'axios';
+import { ArrowDownward, ArrowUpward, Close } from '@mui/icons-material';
 
 interface StoryReelProps {
   myProfile: userDataType,
@@ -24,14 +25,15 @@ export type StoryAsk = {
 
 const StoryReel = ({myProfile} : StoryReelProps) => {
   const [storyList, setStoryList] = useState<StoryType[]>();
+  const [currentStoryIndex, setCurrentStoryIndex] = useState<number | null>(null);
 
   useEffect(() => {
     if (myProfile) {
         const fetchPosts = async () => {
             try {
                 const response = await axios.get<StoryType[]>(`https://socialnetword-fsociety.onrender.com/stories/stories_all`);
-                  const sortedPosts = response.data.sort((a, b) => b.id - a.id);
-                  setStoryList(sortedPosts);
+                const sortedPosts = response.data.sort((a, b) => b.id - a.id);
+                setStoryList(sortedPosts);
             } catch (error) {
                 console.error("Error fetching posts:", error);
             }
@@ -39,17 +41,45 @@ const StoryReel = ({myProfile} : StoryReelProps) => {
 
         fetchPosts();
     }
-}, [myProfile]);
-  
+  }, [myProfile]);
+
+  const openStory = (index: number) => {
+    setCurrentStoryIndex(index);
+  };
+
+  const closeStory = () => {
+    setCurrentStoryIndex(null);
+  };
+
+  const goToNextStory = () => {
+    if (storyList && currentStoryIndex !== null && currentStoryIndex < storyList.length - 1) {
+      setCurrentStoryIndex(currentStoryIndex + 1);
+    }
+  };
+
+  const goToPreviousStory = () => {
+    if (storyList && currentStoryIndex !== null && currentStoryIndex > 0) {
+      setCurrentStoryIndex(currentStoryIndex - 1);
+    }
+  };
+
   return (
     <div className='storyReel'>
-      <CreateStory myProfile = {myProfile}/>
+      <CreateStory myProfile={myProfile} />
       {storyList ? storyList.map(({ author, story }, index) => (
-        <Story key={index} author={author} story={story} />
+        <Story key={index} author={author} story={story} onClick={() => openStory(index)} />
       )) : null}
+
+      {currentStoryIndex !== null && storyList && (
+        <div className="storyModal">
+          <button onClick={closeStory}><Close/></button>
+          <button onClick={goToNextStory}><ArrowUpward/></button>
+          <video src={storyList[currentStoryIndex].story.media} controls autoPlay />
+          <button onClick={goToPreviousStory}><ArrowDownward/></button>         
+        </div>
+      )}
     </div>
   );
 }
 
 export default StoryReel;
-
